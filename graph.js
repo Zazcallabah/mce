e26b5fdbca518fc6cc9b9c5f86a474597b422f36
Peter Hamberg <zazcallabah@gmail.com>
2011-12-24 12:18:37 +0100

    removed superflous code

--- graph.js
+++ graph.js
@@ -1,29 +1,4 @@
-/*
-I stepped through the source, and Step 1 is:
-j = base enchantability
-j2 = 1 + X + X, where X is a random variable with distribution U(0,j/2) (disc.)
-k = j2 + chosen enchant level
-f = (N + N -1) *0.25, where N is U(0,1) (cont.)
-l = k * (1 + f)+.5;
 
-l= (1 + X + X+level) * (1 + ((N + N -1) *0.25))+.5
-
-Which I figure can be approximated to a random variable with normal distribution, mean j/2+level+1.5 and std.dev. sqrt(0.25^2+(j^2/24)^2)
-
-
-	
-	writev("First iteration, base enchantment level = j" );
-	writev(" X is U(0,j/2), E(X) = j/4, V(X) = j^2/48" );
-	writev(" Y is N(m,s), E(Y) = m = j/2 +1, V(Y) = s = j^2/24 " );
-	writev("k = Y + level, k is N(j/2+level+1,j^2/24) ");
-	writev("f is N(1,0.25) ");
-	writev(" final enchantment level = k*f + 0.5, is N(j/2+level+1.5, sqrt(0.25^2/6+(j^2/24)))");
-	
-	var m = ench / 2 + parseInt(level) +1.5;
-	var s = Math.sqrt( ((ench*ench)/24) + (0.25*0.25/6) );
-	
-	write( "F is N( " + m + " , " + s + " )" );
-*/
 function approxSimulDistr( base, level, min, max )
 {
 	var mean  = base/2+level;
