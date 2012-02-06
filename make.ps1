0178b5d034d069e16eea5778a5180131de6649e6
Peter Hamberg <zazcallabah@gmail.com>
2012-02-06 13:30:24 +0100

    added some folder structure and changed to locally hosted jquery

--- make.ps1
+++ make.ps1
@@ -13,14 +13,15 @@ function ReplaceWithFile( $str, $name ) {
 rm mce.html
 
 gc index.html | Foreach-Object{
-$one = ReplaceWithFile $_ "storage.js"
-$two = ReplaceWithFile $one "enchantments.js"
-$three = ReplaceWithFile $two "page.js"
-$four = ReplaceWithFile $three "mc.js"
-$five = ReplaceWithFile $four "iterator.js"
-$six = ReplaceWithFile $five "tools.js"
-$seven = ReplaceWithFile $six "collection.js"
-$eight = ReplaceWithFile $seven "chart.js"
-Add-Content mce.html $eight
+$one = ReplaceWithFile $_ "js/storage.js"
+$two = ReplaceWithFile $one "js/enchantments.js"
+$three = ReplaceWithFile $two "js/page.js"
+$four = ReplaceWithFile $three "js/mc.js"
+$five = ReplaceWithFile $four "js/iterator.js"
+$six = ReplaceWithFile $five "js/tools.js"
+$seven = ReplaceWithFile $six "js/collection.js"
+$eight = ReplaceWithFile $seven "js/chart.js"
+$nine = ReplaceWithFile $eight "js/jquery-1.7.1.min.js"
+Add-Content mce.html $nine
 }
 
