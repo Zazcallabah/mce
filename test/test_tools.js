ce71629ea3391725f1802843e4d203cd831307c6
Peter Hamberg <zazcallabah@gmail.com>
2012-03-24 09:39:36 +0100

    added test for the foreach order bug

--- test/test_tools.js
+++ test/test_tools.js
@@ -1,3 +1,26 @@
 describe('tools', function(){
+	describe('when foreaching over a collection object', function(){
+		it('calls properties in order',function(){
+			var t = makeTools();
+			var c = makeCollection(t);
 
+			c.report(1);
+			c.report(3);
+			c.report(2);
+
+			c.report(3);
+			c.report(3);
+			c.report(2);
+
+			var count = 0;
+			c.foreach( function(item,label)
+			{
+					expect(label).toBe(''+(count+1));
+					expect(item.x).toBe((count+1));
+				count++;
+			}
+			);
+
+		});
+	});
 });
